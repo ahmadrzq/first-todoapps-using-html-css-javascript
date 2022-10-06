8a5929dde34c036128af32243573ab0128f3380f
ahmadrzq <54834305+ahmadrzq@users.noreply.github.com>
2022-10-07 06:58:57 +0700

    Menambahkan Fungsi Memuat Data Ketika Todo Apps Dibuka

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         addTodo();
     });
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
 });
 
 function addTodo() {
@@ -167,3 +171,15 @@ document.addEventListener(SAVED_EVENT, function () {
     console.log(localStorage.getItem(STORAGE_KEY));
 });
 
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+        for (const todo of data) {
+            todos.push(todo);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
